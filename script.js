33f6d93307951442569e9b556ec093d3f4b0fc5a
Michael Owen <mowen0717@gmail.com>
2021-11-26 15:17:27 -0700

    Reset to CPanel Version
    Thanks to git, this will never happen again.

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 /* Adding or Subtracting Nav Bars */
 const toggleButton = document.getElementsByClassName('toggle-button')[0];
 const navbarLinks = document.getElementsByClassName('navbar-links')[0];
+const navbarLinks2 = document.getElementsByClassName('navbar-links')[1];
+const navbarLinks3 = document.getElementsByClassName('navbar-links')[2];
+//const navbarLinks4 = document.getElementsByClassName('navbar-links')[3];
 const organicNavbarLinks = document.getElementsByClassName('organic-navbar-links')[0];
 
 
 toggleButton.addEventListener('click', () => {
 	navbarLinks.classList.toggle('active');
+	navbarLinks2.classList.toggle('active');
+	navbarLinks3.classList.toggle('active');
 	organicNavbarLinks.classList.toggle('active');
 	//navbarLinks4.classList.toggle('active');
 })
