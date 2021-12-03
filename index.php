d7412a62533434f5ba909bfd0736dcd07ac52694
Michael Owen <mowen0717@gmail.com>
2021-12-03 11:14:54 -0700

    Styling for the Index Page | Toggle Button Style Tweaks
    Changed index paragraph styling and toggle button styling to cover more devices.

--- index.php
+++ index.php
@@ -5,30 +5,29 @@
 		<main>
 			<div class="intro-description">
 				<h1>Welcome to Chemistry Trainer</h1>
-				<div class="index-paragraph">
-					<p>Chemistry is an intimidating subject for many people during the best of times. This science tends to mentally break students even when they have consistent access to an instructor in the classroom. Things have gotten even worse in the Covid-19 era, where people are required to learn online and essentially teach themselves. I decided to build this application after tutoring a family member who had to take a chemistry course online due to the pandemic. The standard of instruction was <em>bad</em>. Lesson plans usually consisted of links to decade-old YouTube videos that were not even posted by anyone associated with the college. Instructor feedback on his assignments was rough, consisting of "yes" or "no" responses to his answers with no explanation as to why.</p> 
-
-					<p>I have worked with people from several different universities and this seems to be a common problem. The standard of education is going down in many areas while the cost of tuition keeps rising. I decided to make a website that people could use to learn chemistry at a college-level with great efficiency. If you have a good teacher, this will be a nice supplement, but it likely will not beat what you've got. However, if your instructor is showing up for a payday, this is going to be a great tool for you. It is still a work in progress, but this website is going to make life <em>much</em> easier.</p>
-				</div>
-				
-				
-				<div class="index-paragraph">
-					<h2>How?</h2>
-					<p>This website will cover all of the material that is generally covered in introductory and general college chemistry courses. It will provide concise, clearly-written explanations of the concepts that you are expected to become proficient in applying. However, it will also include many tools at the end of each explanation where you can plug in your values from example problems and see how common problems types are worked through, step-by-step, all the way to your final answer. Any time you feel lost or unsure, you will be able to immediately see exactly how everything works. Using the tools of the website for thity-sixty minutes a day will ensure that you are able to effortlessly walk through your chemistry courses while legitimately learning the concepts. You're not just going to pass, you are going to learn, master, and thrive.</p>
-				</div>
-				
-				
-				<div class="index-paragraph">
-					<h2>A Warning...</h2>
-					<p>This could be a fantastic tool for cheating. I strongly advise that you refrain from using it in that manner. Taking shortcuts will make you weak. Struggling to gain true understanding gives rise to competence. Even if you are not a chemistry major, and you simply have to take the class for your degree, the concepts are similar enough to other fields to be useful throughout life in unexpected ways. Understanding these ideas will also change the way you view the world. You will become more analytical, your critical thinking ability will increase, and you will be less likely to doom yourself to the Hell of Underemployment once you get that degree.</p>
-				</div>
-
-				
-				<div class="index-paragraph">
-					<h2>In Conclusion...</h2>
-					<p>My intention is to add functions to this site weekly. At the bare minimum I will refine older functions so they are more powerful. In the very near future this application will contain everything a student needs to make it through introductory, and general chemistry. A short time after that it will also contain lessons and tools for conquering organic chemistry. At the present time, the website is completely free with no registration required. However, once it is refined, it will be available to the public for $5.00 USD a month with two weeks for free to try it out at no risk or cost to yourself. Best of luck with your studies!<br>--Mike</p>
-				</div>
 			</div>
+
+			<div class="body">
+				<div class="primary-setup">
+					<div class="styled-paragraph">
+						<p>Chemistry is an intimidating subject for many people during the best of times. This science tends to mentally break students even when they have consistent access to an instructor in the classroom. Things have gotten even worse in the Covid-19 era, where people are required to learn online and essentially teach themselves. I decided to build this application after tutoring a family member who had to take a chemistry course online due to the pandemic. The standard of instruction was <em>bad</em>. Lesson plans usually consisted of links to decade-old YouTube videos that were not even posted by anyone associated with the college. Instructor feedback on his assignments was rough, consisting of "yes" or "no" responses to his answers with no explanation as to why.</p> 
+
+						<p>I have worked with people from several different universities and this seems to be a common problem. The standard of education is going down in many areas while the cost of tuition keeps rising. I decided to make a website that people could use to learn chemistry at a college-level with great efficiency. If you have a good teacher, this will be a nice supplement, but it likely will not beat what you've got. However, if your instructor is showing up for a payday, this is going to be a great tool for you. It is still a work in progress, but this website is going to make life <em>much</em> easier.</p>
+
+				
+						<h2>How?</h2>
+						<p>This website will cover all of the material that is generally covered in introductory and general college chemistry courses. It will provide concise, clearly-written explanations of the concepts that you are expected to become proficient in applying. However, it will also include many tools at the end of each explanation where you can plug in your values from example problems and see how common problems types are worked through, step-by-step, all the way to your final answer. Any time you feel lost or unsure, you will be able to immediately see exactly how everything works. Using the tools of the website for thity-sixty minutes a day will ensure that you are able to effortlessly walk through your chemistry courses while legitimately learning the concepts. You're not just going to pass, you are going to learn, master, and thrive.</p>
+
+					
+						<h2>A Warning...</h2>
+						<p>This could be a fantastic tool for cheating. I strongly advise that you refrain from using it in that manner. Taking shortcuts will make you weak. Struggling to gain true understanding gives rise to competence. Even if you are not a chemistry major, and you simply have to take the class for your degree, the concepts are similar enough to other fields to be useful throughout life in unexpected ways. Understanding these ideas will also change the way you view the world. You will become more analytical, your critical thinking ability will increase, and you will be less likely to doom yourself to the Hell of Underemployment once you get that degree.</p>
+
+					
+						<h2>In Conclusion...</h2>
+						<p>My intention is to add functions to this site weekly. At the bare minimum I will refine older functions so they are more powerful. In the very near future this application will contain everything a student needs to make it through introductory, and general chemistry. A short time after that it will also contain lessons and tools for conquering organic chemistry. At the present time, the website is completely free with no registration required. However, once it is refined, it will be available to the public for $5.00 USD a month with two weeks for free to try it out at no risk or cost to yourself. Best of luck with your studies!<br>--Mike</p>
+					</div>
+				</div>
+			</div>	
 		</main>
 	</body>
 </html>
